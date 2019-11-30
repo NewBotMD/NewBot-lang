0082c70ef42de44c6738f8217b96d20064143965
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-11-30 12:33:55 +0300

    Add files via upload

---
+++ lang/en-reply.py
@@ -0,0 +1,36 @@
+doneadd = "done add the group"
+doneadded = "gp alrede added"
+disabled = "gp alrede disable"
+disabl = " done disabl gp"
+
+iscreator = "he is cret"
+setcreator = "done set cret"
+
+issudos = "he is sudos"
+settsudos = "done set sudos"
+
+isowner = "he is owner"
+setowner = "done set owner"
+
+isadmin = "he is admin"
+setadmin = "done set admin"
+
+isvip = "he is vip"
+setvip = "done set vip"
+
+remmsudos = "done rem from soudos"
+remedsudo = "hes not a soudo"
+
+remcreator = "done rem from cr"
+remedcreator ="hes not a cr"
+
+remowner = "done rem from owner"
+remedowner ="hes not a owner"
+
+remadmin = "done rem from admin"
+remedadmin ="hes not a admin"
+
+remvip = "done rem from vip"
+remedvip ="hes not a vip"
+
+userNocc = "no username"
