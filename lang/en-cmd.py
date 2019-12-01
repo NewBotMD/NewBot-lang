17bd612b0b645af7cb7ec70cacd31657e4b1454a
Newbot <58357514+NewBotMD@users.noreply.github.com>
2019-12-01 11:56:08 +0300

    Update en-cmd.py

--- lang/en-cmd.py
+++ lang/en-cmd.py
@@ -1,1 +1,292 @@
-E
+stL = "^lock"
+stU = "^unlock"
+stC = "^un"
+stAd = "enable "
+stUd = "disable "
+startlock = "^lock (.*)$|^unlock (.*)$|enable (.*)$|^disable (.*)$"
+Lurl = "lock links"
+Uurl = "unlock links"
+Lphoto = "lock photo"
+Uphoto = "unlock photo"
+Lvoice = "lock voice"
+Uvoice = "unlock voice"
+Lvideo = "lock video"
+Uvideo = "unlock video"
+Ljoin = "lock nf"
+Ujoin = "unlock nf"
+Linline = "lock inline"
+Uinline = "unlock inline"
+Lchat = "lock chat"
+Uchat = "unlock chat"
+Lcontact = "lock contact"
+Ucontact = "unlock contact"
+Ledits = "lock edits"
+Uedits = "unlock edits"
+Lusername = "lock username"
+Uusername = "unlock username"
+Ltag = "lock tag"
+Utag = "unlock tag"
+Lgifs = "lock gifs"
+Ugifs = "unlock gifs"
+Lfiles = "lock files"
+Ufiles = "unlock files"
+Lsticker = "lock sticker"
+Usticker = "unlock sticker"
+Lmusic = "lock music"
+Umusic = "unlock music"
+Llongtext = "lock longtext"
+Ulongtext = "unlock longtext"
+Lflood = "enable flood"
+Uflood = "disable flood"
+Larabic = "lock arabic"
+Uarabic = "unlock arabic"
+Lenglish = "lock english"
+Uenglish = "unlock english"
+Lbots = "lock bots"
+Ubots = "unlock bots"
+Lmarkdown = "lock markdown"
+Umarkdown = "unlock markdown"
+Lfwd = "lock fwd"
+Ufwd = "unlock fwd"
+Lpin = "enable pin"
+Upin = "disable pin"
+Lall = "lock all"
+Uall = "unlock all"
+Lnote = "lock note"
+Unote = "unlock note"
+
+LwelcomeSend = "enable welcome"
+UwelcomeSend = "disable welcome"
+
+Lreply = "enable replys"
+Ureply = "disable replys"
+
+Laudo = "enable audoadd"
+Uauto = "disable audoadd"
+
+Lkickme = "enable kickme"
+Ukickme = "disable kickme"
+
+LreplyBOT = "enable replysbot"
+UreplyBOT = "disable replysbot"
+
+Lreplysbot = "enable id"
+Ureplysbot = "disable id"
+
+LreplysbotPH = "^enable id photo$"
+UreplysbotPH = "^disable id photo$"
+
+ShowSudos = "^sudos$"
+setSudoC = "^setsudosorder (.*)$"
+RsetSudoC = "setsudosorder "
+
+STbanall = "Bans all"
+STtkall = "Mutes all"
+
+STlink = "Links"
+STphoto = "Photo"
+STvoice = "Voice"
+STvideo = "Video"
+STjoin = "Nf"
+STinline = "Inline"
+STchat = "Chat"
+STcontact = "Contact"
+STedits = "Edits"
+STusername = "Username"
+STtag = "Tag"
+STgifs = "Gifs"
+STfiles = "Files"
+STsticker = "Sticker"
+STmusic = "Music"
+STlongtext = "Longtext"
+STflood = "Flood"
+STarabic = "Arabic"
+STenglish = "English"
+STbots = "Bots"
+STmarkdown = "Markdown"
+STfwd = "Fwd"
+STpin = "Pin"
+
+STwelcomeSend = "Welcome"
+STReplySend = "Replys"
+STReplySendBOT = "Botreplys"
+STkickme = "Kickme"
+STIDSend = "ID"
+
+STreplysbotSend = "replysbot"
+
+STall = "All"
+STnote = "Note"
+STword = "Words"
+
+
+STprivates = "privates"
+STgroup = "groups"
+
+
+STreply = "^addreply (.*)"
+DLreply = "^remreply (.*)"
+RPreply = "addreply "
+RPdreply = "remreply "
+
+STreplyBOT = "^addreplybot (.*)"
+DLreplyBOT = "^remreplybot (.*)"
+RPreplyBOT = "addreplybot "
+RPdreplyBOT = "remreply "
+
+
+
+
+
+
+add = "add"
+disabl = "remadd"
+setcreator = "^set creator$|^set creator @(.*)$|^set creator [0-9]+$"
+setcreator2 = "^set creator [0-9]+$"
+
+
+remcreator ="^rem creator$|^rem creator @(.*)$|^rem creator [0-9]+$"
+remcreator2 = "^rem creator [0-9]+$"
+
+setsudos = "^set sudo$|^set sudo @(.*)$|^set sudo [0-9]+$"
+setsudos2 = "^set sudo [0-9]+$"
+remsudos = "^rem sudo$|^rem sudo @(.*)$|^rem sudo [0-9]+$"
+remsudos2 = "^rem sudo [0-9]+$"
+
+setadmin = "^set admin$|^set admin @(.*)$|^set admin [0-9]+$"
+setadmin2 = "^set admin [0-9]+$"
+remadmin = "^rem admin$|^rem admin @(.*)$|^rem admin [0-9]+$"
+remadmin2 = "^remadmin [0-9]+$"
+
+setvip = "^set vip$|^set vip @(.*)$|^set vip [0-9]+$"
+setvip2 = "^set vip [0-9]+$"
+remvip = "^rem vip$|^rem vip @(.*)$|^rem vip [0-9]+$"
+remvip2 = "^rem vip [0-9]+$"
+
+setowner = "^set owner$|^set owner @(.*)$|^set owner [0-9]+$"
+setowner2 = "^set owner [0-9]+$"
+remowner = "^rem owner$|^rem owner @(.*)$|^rem owner [0-9]+$"
+remowner2 = "^rem owner [0-9]+$"
+
+ban = "^ban$|^ban @(.*)$|^ban [0-9]+$"
+ban2 = "^ban [0-9]+$"
+
+
+banall = "^unbanall$|^unbanall @(.*)$|^unbanall [0-9]+$"
+ban2all = "^unbanall [0-9]+$"
+
+TK = "^mute$|^mute @(.*)$|^mute [0-9]+$"
+TK2 = "^mute [0-9]+$" 
+
+TKall = "^muteall$|^muteall @(.*)$|^muteall [0-9]+$"
+TK2all = "^muteall [0-9]+$"
+
+unTKall = "^unmuteall$|^unmuteall @(.*)$|^unmuteall [0-9]+$"
+unTK2all = "^unmuteall [0-9]+$"
+
+FloodM = "^flood msgs [0-9]+$"
+FloodT = "^flood time [0-9]+$"
+
+
+unban = "^unban$|^unban @(.*)$|^unban [0-9]+$"
+unban2 = "^unban [0-9]+$"
+
+unbanall = "^unbanall$|^unbanall @(.*)$|^unbanall [0-9]+$"
+unban2all = "^unbanall [0-9]+$"
+
+unTK = "^unmute$|^unmute @(.*)$|^unmute [0-9]+$"
+unTK2 = "^unmute [0-9]+$"
+
+bans = "^bans$"
+restricteds = "^mutes$"
+
+
+
+
+block = "^block (.*)$|^block$"
+block2 = "^block (.*)$"
+
+
+block = "^block (.*)|^block"
+block2 ="^block (.*)"
+ 
+unblock = "^unblock (.*)$|^unblock$"
+unblock2 = "^unblock (.*)$"
+
+Blocklist = "^listblock$|^blocks$"
+
+admins = "^listadmin$|^admins$"
+vips = "^listvip$|^vips$"
+owners = "^listowner$|^owners$"
+sudosList = "^listsudo$|^sudos$"
+creators = "^listcreator$|^creators$"
+
+PROadmins = "^promote admins$|^setadmins$"
+
+
+RPwel = "setwelcome "
+RPbk = "block "
+RPubk = "unblock "
+
+settingsCmd = "settings"
+
+STWEL = "^setwelcome (.*)$"
+
+
+Replylist= "^replylist$|^replys$"
+
+ReplylistBOT= "^replybotlist$|^replysbot$"
+
+
+Alllist = "^alllist$"
+
+setname = "^setname (.*)$"
+Dsetname = "setname "
+
+setabout= "^setabout (.*)$"
+Dsetabout = "setabout "
+
+setphoto = "^setphoto$"
+
+mymsgs = "mymsgs"
+
+myedits = "myedits"
+
+myaddcontact = "mycontact"
+
+
+kickme = "kickme"
+replysbot = "replysbot"
+
+
+pinmsg = "^pin$"
+unpinmsg = "^unpin$"
+
+stats = "^groups$"
+
+SETlink = "^setlink ([Hh][Tt][Tt][Pp][Ss]:/|[Hh][Tt][Tt][Pp]://)(.*)"
+RPlink = "setlink "
+link = "link"
+
+sendall = "^send$"
+fwdall = "^fwd$"
+
+showGPS = "^groups links$"
+
+Setauto = "autoadd [0-9]+$"
+
+leaveChat = "^leave -(.*)$"
+leaveChatS = "^leave$"
+
+deleteDeleted = "^kick deleted$" 
+
+ShowO = "^orders$|^help$"
+
+replysbotus = "^replysbot @(.*)$"
+Ulang = "update lang"
+
+
+Chlang = "change lang"
+
+ID = "id"
+idus = "^id @(.*)$"
